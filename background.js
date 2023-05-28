b88ead0f09031fba735fe1cbf8265b7c12cf0e5c
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2023-05-28 19:45:22 +0200

    reduced twinkles in background

--- background.js
+++ background.js
@@ -1,14 +1,35 @@
 function fn() {
-    window.requestAnimFrame = (function () {
-        return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || function (callback) {
-                    window.setTimeout(callback, 1000 / 60);
-                }
+    window.requestAnimFrame = (function() {
+        return (
+            window.requestAnimationFrame ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame ||
+            function(callback) {
+                window.setTimeout(callback, 1000 / 60);
+            }
+        );
     })();
-    var canvas = document.getElementById('canvas'), ctx = canvas.getContext('2d'), w = canvas.width = window.innerWidth, h = canvas.height = window.innerHeight, hue = 217, stars = [], count = 0, maxStars = 1200;
-    var canvas2 = document.createElement('canvas'), ctx2 = canvas2.getContext('2d');
+    var canvas = document.getElementById('canvas'),
+        ctx = canvas.getContext('2d'),
+        w = (canvas.width = window.innerWidth),
+        h = (canvas.height = window.innerHeight),
+        hue = 217,
+        stars = [],
+        count = 0,
+        maxStars = 1200;
+    var canvas2 = document.createElement('canvas'),
+        ctx2 = canvas2.getContext('2d');
     canvas2.width = 100;
     canvas2.height = 100;
-    var half = canvas2.width / 2, gradient2 = ctx2.createRadialGradient(half, half, 0, half, half, half);
+    var half = canvas2.width / 2,
+        gradient2 = ctx2.createRadialGradient(
+            half,
+            half,
+            0,
+            half,
+            half,
+            half
+        );
     gradient2.addColorStop(0.025, '#fff');
     gradient2.addColorStop(0.1, 'hsl(' + hue + ', 61%, 33%)');
     gradient2.addColorStop(0.25, 'hsl(' + hue + ', 64%, 6%)');
@@ -20,22 +41,21 @@ function fn() {
     function random(min, max) {
         if (arguments.length < 2) {
             max = min;
-            min = 0
+            min = 0;
         }
         if (min > max) {
             var hold = max;
             max = min;
-            min = hold
+            min = hold;
         }
-        return Math.floor(Math.random() * (max - min + 1)) + min
+        return Math.floor(Math.random() * (max - min + 1)) + min;
     }
-
     function maxOrbit(x, y) {
-        var max = Math.max(x, y), diameter = Math.round(Math.sqrt(max * max + max * max));
-        return diameter / 2
+        var max = Math.max(x, y),
+            diameter = Math.round(Math.sqrt(max * max + max * max));
+        return diameter / 2;
     }
-
-    var Star = function () {
+    var Star = function() {
         this.orbitRadius = random(maxOrbit(w, h));
         this.radius = random(60, this.orbitRadius) / 12;
         this.orbitX = w / 2;
@@ -44,21 +64,23 @@ function fn() {
         this.speed = random(this.orbitRadius) / 900000;
         this.alpha = random(2, 10) / 10;
         count++;
-        stars[count] = this
+        stars[count] = this;
     };
-    Star.prototype.draw = function () {
-        var x = Math.sin(this.timePassed) * this.orbitRadius + this.orbitX, y = Math.cos(this.timePassed) * this.orbitRadius + this.orbitY, twinkle = random(10);
-        if (twinkle === 1 && this.alpha > 0) {
-            this.alpha -= 0.05
-        } else if (twinkle === 2 && this.alpha < 1) {
-            this.alpha += 0.05
-        }
+    Star.prototype.draw = function() {
+        var x = Math.sin(this.timePassed) * this.orbitRadius + this.orbitX,
+            y = Math.cos(this.timePassed) * this.orbitRadius + this.orbitY;
         ctx.globalAlpha = this.alpha;
-        ctx.drawImage(canvas2, x - this.radius / 2, y - this.radius / 2, this.radius, this.radius);
-        this.timePassed += this.speed
+        ctx.drawImage(
+            canvas2,
+            x - this.radius / 2,
+            y - this.radius / 2,
+            this.radius,
+            this.radius
+        );
+        this.timePassed += this.speed;
     };
     for (var i = 0; i < maxStars; i++) {
-        new Star()
+        new Star();
     }
     function animation() {
         ctx.globalCompositeOperation = 'source-over';
@@ -66,13 +88,16 @@ function fn() {
         ctx.fillStyle = 'hsla(' + hue + ', 64%, 6%, 1)';
         ctx.fillRect(0, 0, w, h);
         ctx.globalCompositeOperation = 'lighter';
-        for (var i = 1, l = stars.length; i < l; i++) {
-            stars[i].draw()
+
+        for (var i =1, l = stars.length; i < l; i++) {
+            stars[i].draw();
         }
-        ;
-        window.requestAnimationFrame(animation)
+
+        window.requestAnimationFrame(animation);
     }
 
     animation();
+
 }
+
 fn();
